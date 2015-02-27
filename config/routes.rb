57f8646ea2032acb91bc23aa0aad0c7088f9ebcd
Tim Renken <tim@timrenken.io>
2015-02-26 19:16:23 -0600

    added users index

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'registrations'}
   root 'pages#home'
   
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
 end
