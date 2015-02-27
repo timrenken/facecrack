f2caa270f70905219938b8c26551c9aa1d49ad03
Tim Renken <tim@timrenken.io>
2015-02-26 19:30:38 -0600

    added database seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "tim2", email: "tim2@timrenken.io", password: "Remember1", password_confirmation: "Remember1")
+User.create(username: "tim3", email: "tim3@timrenken.io", password: "Remember1", password_confirmation: "Remember1")
+User.create(username: "tim4", email: "tim4@timrenken.io", password: "Remember1", password_confirmation: "Remember1")
+User.create(username: "tim5", email: "tim5@timrenken.io", password: "Remember1", password_confirmation: "Remember1")
+p "Users created"
